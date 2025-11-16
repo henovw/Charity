93bf8a1a8005ad0f615c63323625f662ee27b315
henovw <henryvanweelderen@gmail.com>
2025-11-15 23:11:13 -0800

    main map view added

---
+++ src/components/MainMap.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import GoogleMapReact from 'google-map-react';
+import './MainMap.css'
+import MiniDesc from "./MiniDesc"
+
+const ImageMarker = ({ img, onClick }) => (
+    <div className="iconDiv" onClick={onClick}>
+      <img 
+        className="iconImage"
+        src={img}
+        alt="marker"
+      />
+    </div>
+  );
+
+function MainMap(props) {
+    const [selected, setSelected] = useState(null)
+
+    const defaultProps = {
+        center: {
+          lat: 49.267535,
+          lng: -123.128936
+        },
+        zoom: 13
+      };
+    
+    return (
+        <div className="mainContainer">
+    
+
+
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyD2lWRF2-QLrl0EUlwm3m_q50xmLD-1Yos" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+        >
+        {props.list.map((item) => (
+            <ImageMarker lat={item.lat} lng={item.lng} img={item.img}
+                onClick={() => setSelected(item)}
+            />
+        ))}
+
+        {selected && <MiniDesc
+            name={selected.name}
+            description={selected.description}
+            onClose={() => setSelected(null)}
+            lat={selected.lat}
+            lng={selected.lng}
+            img={selected.img}
+            categories={selected.categories}
+            needs={selected.needs}
+            link={selected.link}
+    
+        />}
+
+          </GoogleMapReact>
+        </div>
+      );
+    }
+
+export default MainMap
---
+++ src/components/MiniDesc.jsx
@@ -0,0 +1,39 @@
+import "./MiniDesc.css"
+
+function MiniDesc(props) {
+    return (
+        <div>
+            
+        <div className="box">
+        <button className="exit" onClick={props.onClose}><img src="src/assets/xbutton.png"></img></button>
+            <img src={props.img}></img>
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <br></br>
+            <div class="categories">
+                
+            {props.categories.map(item => (
+                <p className="category" key={item}>{item}</p>
+            ))}
+            </div>
+                <br></br>
+            <h2>We need:</h2>
+            <div className="categories">
+            {props.needs.map(item => (
+                <p className="need" key={item}>{item}</p>
+            ))}
+            </div>
+
+                <br></br>
+            <button className="link">
+                <a href={props.link} target="_blank">Donate 🥰</a>
+            </button>
+
+
+            
+        </div>
+        </div>
+    )
+}
+
+export default MiniDesc
